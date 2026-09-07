e2893ee46dc932b9b87a06e8796d37f7cae0087a
Tharika P <tharikaleet06@gmail.com>
2026-09-07 23:39:23 +0530

    Create Solution.java

---
+++ Two-Pointers/Opposite Direction/11. Container With Most Water/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l=0;
+        int r=height.length-1;
+        int max=0;
+        while(l<r){
+             int water=(Math.min(height[l],height[r]))*(r-l);
+             max=Math.max(max,water);
+            if(height[l]<height[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return max;
+    }
+}
